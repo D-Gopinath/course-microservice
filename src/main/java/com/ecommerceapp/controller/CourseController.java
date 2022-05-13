2291171ba3f6c4296acde0985e1d79f495a6d093
D-Gopinath <gopinathd3737@gmail.com>
2022-05-13 15:50:13 +0530

    added procfile and system properties

--- src/main/java/com/ecommerceapp/controller/CourseController.java
+++ src/main/java/com/ecommerceapp/controller/CourseController.java
@@ -1,6 +1,5 @@
 package com.ecommerceapp.controller;
 
-import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
